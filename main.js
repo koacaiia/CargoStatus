c7e1b7b46f721bbd0cce96ad0cc9f9cf3715dd74
koaca <koacaiia@gmail.com>
2025-02-04 20:42:27 +0900

    0204

---
+++ main.js
@@ -0,0 +1,43 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDLzmZyt5nZwCk98iZ6wi01y7Jxio1ppZQ",
+    authDomain: "fine-bondedwarehouse.firebaseapp.com",
+    databaseURL: "https://fine-bondedwarehouse-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fine-bondedwarehouse",
+    storageBucket: "fine-bondedwarehouse.appspot.com",
+    messagingSenderId: "415417723331",
+    appId: "1:415417723331:web:15212f190062886281b576",
+    measurementId: "G-SWBR4359JQ"
+};
+if(firebase.apps.length==0){
+  firebase.initializeApp(firebaseConfig);
+}
+else{firebase.app();}
+// const doc =document.documentElement;
+// function fullScreen(){
+//   doc.requestFullscreen();
+// }
+// fullScreen();
+const database_f = firebase.database();
+const messaging = firebase.messaging();
+const storage_f = firebase.storage();
+const deptName = "WareHouseDept2";
+const dateDiv = document.querySelector("#titleDate");
+const dateT = (d)=>{
+    let result_date;
+    try{
+    let result_month = d.getMonth()+1;
+    let result_day =d.getDate();
+    if(result_month<10){
+        result_month ="0"+result_month;
+    };
+    if(result_day <10){
+        result_day ="0"+result_day;
+    };
+    result_date = d.getFullYear()+"-"+result_month+"-"+result_day;
+    return result_date;
+    }catch(e){
+    return result_date ="미정";
+    }
+};
+
+dateDiv.value = dateT(new Date());
